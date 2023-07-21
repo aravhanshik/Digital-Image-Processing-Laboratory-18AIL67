409c2818f8c2d106c7782b351c9c37ef19fd4899
Arav Hanshik <80516330+aravhanshik@users.noreply.github.com>
2023-07-21 13:12:06 +0530

    Add files via upload

---
+++ 2 Rotate Scale Translate/Rotate_Scale_Translate.m
@@ -0,0 +1,17 @@
+OriginalImage = imread('C:\Users\Student\Downloads\bug.jpg');
+subplot(2,2,1); imshow(OriginalImage);
+title("Original Image");
+
+TranslateImage = imtranslate(OriginalImage, [50, 50], "OutputView", "full");
+subplot(2,2,2); imshow(TranslateImage);
+title("Translated Image")
+
+scale = 0.5;
+ScaledImage = imresize(TranslateImage, scale);
+subplot(2,2,3); imshow(ScaledImage);
+title("Translated Image + Scaled Image")
+
+deg = 25;
+RotatedImage = imrotate(ScaledImage, deg);
+subplot(2,2,4); imshow(RotatedImage);
+title("Translated Image + Scaled Image + Rotated Image")
